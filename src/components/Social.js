64c03dbd1642e15fccac24af097932bef36e9b14
Cristian Sanchez <cristian.manu7@gmail.com>
2022-11-03 00:26:55 -0600

    social update

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,18 +3,53 @@ import { Button, Icon } from 'semantic-ui-react'
 
 const ButtonExampleSocial = (props) => (
   <div className="social">
-    <Button color="twitter">
-      <Icon name="twitter" /> Twitter
-    </Button>
+    <a
+      href="https://twitter.com/Cristian_14715"
+      alt="link"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Button color="twitter">
+        <Icon name="twitter" /> Twitter
+      </Button>
+    </a>
+    <a
+      href="https://www.linkedin.com/feed/"
+      alt="link"
+      target="_blank"
+      rel="noreferrer"
+    ></a>
     <Button color="linkedin">
       <Icon name="linkedin" /> LinkedIn
     </Button>
+    <a
+      href="https://www.instagram.com/cristian_snchez7/"
+      alt="link"
+      target="_blank"
+      rel="noreferrer"
+    ></a>
     <Button color="instagram">
       <Icon name="instagram" /> Instagram
     </Button>
+    <a
+      href="https://www.youtube.com/channel/UCdXKvZmgrU3sWxtYEOKNBVg"
+      alt="link"
+      target="_blank"
+      rel="noreferrer"
+    ></a>
     <Button color="youtube">
       <Icon name="youtube" /> YouTube
     </Button>
+    <a
+      href="https://www.youtube.com/channel/UCdXKvZmgrU3sWxtYEOKNBVg"
+      alt="link"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <Button color="github">
+        <Icon name="github" /> GitHub
+      </Button>
+    </a>
   </div>
 )
 
