e6d384fdee3ec0269a9472e29cf309f3f13f27b2
Cristian Sanchez <cristian.manu7@gmail.com>
2022-11-02 21:42:24 -0600

    social update

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,17 +3,8 @@ import { Button, Icon } from 'semantic-ui-react'
 
 const ButtonExampleSocial = () => (
   <div className="social">
-    <Button color="facebook">
-      <Icon name="facebook" /> Facebook
-    </Button>
     <Button color="twitter">
       <Icon name="twitter" /> Twitter
-    </Button>
-    <Button color="google plus">
-      <Icon name="google plus" /> Google Plus
-    </Button>
-    <Button color="vk">
-      <Icon name="vk" /> VK
     </Button>
     <Button color="linkedin">
       <Icon name="linkedin" /> LinkedIn
